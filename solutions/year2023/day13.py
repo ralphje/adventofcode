22e1b4a3001c29643f0c30a6b65609cd22a6254d
Ralph Broenink <ralph@ralphbroenink.net>
2023-12-13 18:24:25 +0100

    Add day 13

---
+++ solutions/year2023/day13.py
@@ -0,0 +1,44 @@
+"""This file holds the solutions for Advent of Code 2023 day 13: Point of Incidence
+https://adventofcode.com/2023/day/13
+"""
+
+
+def _find_mirror_point(lines: list[str], smudges: int = 0) -> int:
+    return next(
+        (
+            # Return the first valid mirror point
+            i
+            # Iterated over all possible mirror points
+            for i in range(1, len(lines))
+            # Where...
+            if sum(
+                # Count the amount of times the character pairs do not match
+                # (Note: True == 1)
+                c1 != c2
+                # Iterated over line pairs, as split on line i, first part reversed
+                for l1, l2 in zip(lines[i - 1 :: -1], lines[i:])
+                # Iterated over each character pair in each line pair
+                for c1, c2 in zip(l1, l2)
+            )
+            # This must equal the amount of smudges
+            == smudges
+        ),
+        # Return 0 if no mirror point was found
+        0,
+    )
+
+
+def part_1(blocks: str, smudges: int = 0) -> int:
+    """Solution for Advent of Code 2023 day 13 part 1"""
+    return sum(
+        # amount of horizontal mirror points
+        _find_mirror_point(block, smudges) * 100
+        # amount of vertical mirror points
+        + _find_mirror_point(list(zip(*block)), smudges)
+        for block in [s.splitlines() for s in blocks.split("\n\n")]
+    )
+
+
+def part_2(blocks: str) -> int:
+    """Solution for Advent of Code 2023 day 13 part 2"""
+    return part_1(blocks, smudges=1)
